08494a3e73b22017838e0b0879f412428608e9ca
chlsmile <chl_smile@163.com>
2016-03-30 15:53:57 +0800

    test

---
+++ manage-service/src/main/java/com/chlsmile/manage/App.java
@@ -0,0 +1,13 @@
+package com.chlsmile.manage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
