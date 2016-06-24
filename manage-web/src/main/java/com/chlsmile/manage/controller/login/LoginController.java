8f5b5c21b925b7ad0dd1fd2867607ad088d50d47
hualun <hualun@mogujie.com>
2016-06-24 16:42:22 +0800

    add

---
+++ manage-web/src/main/java/com/chlsmile/manage/controller/login/LoginController.java
@@ -0,0 +1,13 @@
+package com.chlsmile.manage.controller.login;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Function:
+ * User: hualun
+ * Date: 16/6/24 下午4:34
+ */
+@Controller
+public class LoginController {
+}
