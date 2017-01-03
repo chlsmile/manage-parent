b6359a4131a8ef17225484d35c5386d99b57bb19
hualun <hualun@mogujie.com>
2017-01-03 10:10:01 +0800

    add file

---
+++ manage-service/src/main/java/com/chlsmile/manage/common/utils/DateTimeUtil.java
@@ -0,0 +1,10 @@
+package com.chlsmile.manage.common.utils;
+
+/**
+ * Created with: IntelliJ IDEA.
+ * Function: 日期工具类
+ * User: chl_smile
+ * Date: 2016-06-28 16:04:00
+ */
+public class DateTimeUtil {
+}
