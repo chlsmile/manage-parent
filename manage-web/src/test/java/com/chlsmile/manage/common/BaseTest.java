de63ce9ee1c64db96a5682dd19644fa48ac58c21
hualun <hualun@mogujie.com>
2017-01-08 15:46:27 +0800

    add

---
+++ manage-web/src/test/java/com/chlsmile/manage/common/BaseTest.java
@@ -0,0 +1,15 @@
+package com.chlsmile.manage.common;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Function:
+ * User: chl_smile@163.com
+ */
+@ContextConfiguration(locations = {"classpath:applicationContext-manage.xml"})
+@RunWith(SpringJUnit4ClassRunner.class)
+public class BaseTest extends AbstractJUnit4SpringContextTests{
+}
